569e8daecb497ce3a215c0c0e94b3d749d3f7ee1
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-18 23:48:00 +0530

    variables and functions that we need⏱️⏱️⏱️

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// variables and functions that we need!
+
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+
+}
+
+function stop() {
+
+}
+
+function restart() {
+
+}
+// another function to update the changes in display
+function update() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
