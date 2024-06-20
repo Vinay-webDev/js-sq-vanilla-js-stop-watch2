85262b5c292d31f1973f5f1b766689c856c7abc5
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-20 23:44:53 +0530

    functions that needed

--- js/script.js
+++ js/script.js
@@ -6,10 +6,21 @@ let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
 
+function start() {
 
+}
 
+function stop() {
 
+}
 
+function restart() {
+
+}
+
+function update() {
+    
+}
 
 
 
